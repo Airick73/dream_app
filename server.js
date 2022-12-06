38100b7d98ea79d60dd648dfddb375510bf850af
Eric Kemmer <eric199900@gmail.com>
2022-12-06 09:33:29 -0500

    Adding server.js to serve our app via express

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-//importing from .env and setting up config
+//importing from .env file
 import * as dotenv from 'dotenv';
+//loads .env content into process.env
 dotenv.config();
 
 //importing OpenAI, initializing open AI sdk, 
@@ -21,4 +22,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post('/dream');
+app.post('/dream', async(req, res) => {
+    
+    const prompt = req.body.prompt;
+
+    const aiResponse = await openai.createImage({
+        prompt,
+        n: 1,
+        size: '1024x1024',
+    });
+
+    const image  = aiResponse.data.data[0].url;
+    res.send({ image });
+});
+
+app.listen(8080, () => console.log('Make art on http://localhost:8080/dream'));
